c15c7736486622450960f4bbb6687e718e8070f5
Wighawag <wighawag@gmail.com>
2019-11-23 17:02:36 +0000

    add cryptokitty + contract

--- src/mapping.ts
+++ src/mapping.ts
@@ -14,82 +14,88 @@ function toBytes(hexString: String): Bytes {
     return result as Bytes;
 }
 
-// function supportsInterface(contract: EIP721, interfaceId: String, expected : boolean = true) : boolean {
-//     let supports = contract.try_supportsInterface(toBytes(interfaceId));
-//     return !supports.reverted && supports.value == expected;
-// }
-
 function supportsInterface(contract: EIP721, interfaceId: String, expected : boolean = true) : boolean {
-    let result = contract.tryCall('supportsInterface', [EthereumValue.fromFixedBytes(toBytes(interfaceId))]);
-    if (result.reverted) {
-        return false;
-    }
-    let value = result.value;
-    if(value.length == 0) {
-        return !expected;
-    }
-    let firstValue = value[0];
-    // TODO check length
-    // TODO : 
-    let byteValue = changetype<Bytes>(value[0].data as u32);
-    if(byteValue.length == 0) {
-        return !expected;
-    }
-    // if(byteValue.toHexString() == '0x' || byteValue.toHexString() == '0x0') {
-    //     return !expected;
-    // }
-    
-    return firstValue.toBoolean() == expected;
-    
-    // if(byteValue.toHex() != '0x1') {
-    //     // log.debug('truthful != 0x1 : {}',[byteValue.toHexString()]);
-    // }
-
-    // return expected;
+    let supports = contract.try_supportsInterface(toBytes(interfaceId));
+    return !supports.reverted && supports.value == expected;
 }
+
+// function supportsInterface(contract: EIP721, interfaceId: String, expected : boolean = true) : boolean {
+//     let result = contract.tryCall('supportsInterface', [EthereumValue.fromFixedBytes(toBytes(interfaceId))]);
+//     if (result.reverted) {
+//         return false;
+//     }
+//     let value = result.value;
+//     if(value.length == 0) {
+//         return !expected;
+//     }
+//     let firstValue = value[0];
+//     // TODO check length
+//     // TODO : 
+//     let byteValue = changetype<Bytes>(value[0].data as u32);
+//     if(byteValue.length == 0) {
+//         return !expected;
+//     }
+//     // if(byteValue.toHexString() == '0x' || byteValue.toHexString() == '0x0') {
+//     //     return !expected;
+//     // }
+    
+//     return firstValue.toBoolean() == expected;
+    
+//     // if(byteValue.toHex() != '0x1') {
+//     //     // log.debug('truthful != 0x1 : {}',[byteValue.toHexString()]);
+//     // }
+
+//     // return expected;
+// }
 
 export function handleTransfer(event: Transfer): void {
     let contract = EIP721.bind(event.address);
     let contractInfo = Contract.load(event.address.toHex());
     if(contractInfo == null) {
-        contractInfo = new Contract(event.address.toHex());
-        log.debug('contract : {}',[event.address.toHexString()]);
+        log.info('contract : {}',[event.address.toHexString()]);
         let supportsEIP165Identifier = supportsInterface(contract, '01ffc9a7');
-        log.debug('eip165 : {}', [supportsEIP165Identifier ? 'true' : 'false']);
+        // log.debug('eip165 : {}', [supportsEIP165Identifier ? 'true' : 'false']);
         let supportsEIP721Identifier = supportsInterface(contract, '80ac58cd');
-        log.debug('eip721 : {}', [supportsEIP721Identifier ? 'true' : 'false']);
+        // log.debug('eip721 : {}', [supportsEIP721Identifier ? 'true' : 'false']);
+        let supportsCryptoKittiesIdentifier = supportsInterface(contract, '9a20483d');
+        // log.debug('cryptoKitties : {}', [supportsCryptoKittiesIdentifier ? 'true' : 'false']);
         let supportsNullIdentifierFalse = supportsInterface(contract, '00000000', false);
-        log.debug('eip165Null : {}', [supportsNullIdentifierFalse ? 'true' : 'false']);
+        // log.debug('eip165Null : {}', [supportsNullIdentifierFalse ? 'true' : 'false']);
         let supportsEIP721 = supportsEIP165Identifier &&
-            supportsEIP721Identifier &&
+            (supportsCryptoKittiesIdentifier || supportsEIP721Identifier) &&
             supportsNullIdentifierFalse;
 
         let supportsEIP721Metadata = false;
         if(supportsEIP721) {
             supportsEIP721Metadata = supportsInterface(contract, '5b5e139f');
-            log.debug('eip721Metadata : {}', [supportsEIP721Metadata ? 'true' : 'false']);
+            // log.debug('eip721Metadata : {}', [supportsEIP721Metadata ? 'true' : 'false']);
         }
-        contractInfo.supportsEIP721 = supportsEIP721;
-        contractInfo.supportsEIP721Metadata = supportsEIP721Metadata;
-        contractInfo.save();
+        if (supportsEIP721) {
+            contractInfo = new Contract(event.address.toHex());
+            contractInfo.supportsCryptoKittyStandard = supportsCryptoKittiesIdentifier;
+            contractInfo.supportsEIP721Metadata = supportsEIP721Metadata;
+            contractInfo.address = event.address;
+            contractInfo.save();
+        } else {
+            return;
+        }
     }
-    
-    
-    if(!contractInfo.supportsEIP721) {
-        return;
-    }
-    
 
     let tokenId = event.params.id;
     let id = event.address.toHex() + '_' + tokenId.toHex();
     let eip721Token = EIP721Token.load(id);
     if(eip721Token == null) {
         eip721Token = new EIP721Token(id);
-        eip721Token.contractAddress = event.address;
+        eip721Token.contract = contractInfo.id;
         eip721Token.tokenID = tokenId;
         eip721Token.mintTime = event.block.timestamp;
         if (contractInfo.supportsEIP721Metadata) {
             let metadataURI = contract.try_tokenURI(tokenId);
+            if(!metadataURI.reverted) {
+                eip721Token.tokenURI = metadataURI.value; // only set it at creation
+            }
+        } else if (contractInfo.supportsCryptoKittyStandard) {
+            let metadataURI = contract.try_tokenMetadata(tokenId, "");
             if(!metadataURI.reverted) {
                 eip721Token.tokenURI = metadataURI.value; // only set it at creation
             }
