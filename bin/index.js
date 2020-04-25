d54096fdf8e31870702cb987040f60a33be5ccf6
Wighawag <wighawag@gmail.com>
2020-04-25 19:21:04 +0100

    add bin script

---
+++ bin/index.js
@@ -0,0 +1,7 @@
+const graphCLI = require("@graphprotocol/graph-cli");
+const path = require('path');
+
+(async () => {
+    process.chdir(path.join(__dirname, '/..'));
+    graphCLI.run(process.argv.slice(2));
+})();
