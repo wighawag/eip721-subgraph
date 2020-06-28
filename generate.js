c869bc77987b94f629551f19ae134908d70fe2f4
Wighawag <wighawag@gmail.com>
2020-06-28 18:26:33 +0100

    use __dirname for generate.js

--- generate.js
+++ generate.js
@@ -1,11 +1,14 @@
 const fs = require("fs-extra");
+const path = require("path");
 const Handlebars = require("handlebars");
 
 const args = process.argv.slice(2);
 const chainName = args[0];
 const startBlock = args[1];
-// # startBlock: 6367157 # erro on 0x00 string 
-// # startBlock: 5806610 #PoloDod (0x7b70405fba653cc8eff5afad1c5079d7da0a0247) 
-const template = Handlebars.compile(fs.readFileSync("./templates/subgraph.yaml").toString());
-const result = template({chainName, startBlock});
-fs.writeFileSync("./subgraph.yaml", result);
+// # startBlock: 6367157 # erro on 0x00 string
+// # startBlock: 5806610 #PoloDod (0x7b70405fba653cc8eff5afad1c5079d7da0a0247)
+const template = Handlebars.compile(
+  fs.readFileSync(path.join(__dirname, "templates/subgraph.yaml")).toString()
+);
+const result = template({ chainName, startBlock });
+fs.writeFileSync(path.join(__dirname, "subgraph.yaml"), result);
